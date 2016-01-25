60efeb618a439d8f24ccff1ad5a704ba1f24a7c6
Robbie Fryers <Robbie@Robbies-MacBook-Pro.local>
2016-01-25 17:59:42 +0000

    tester

---
+++ TeamE.java
@@ -0,0 +1,17 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * The main class
+ */
+public class TeamE {
+	/**
+	 * The main method
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		TimeTableGUI display = new TimeTableGUI();
+		display.setVisible(true);
+	}
+}
